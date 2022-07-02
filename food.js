556b4449ef3b533c44c69ea046eab702f1fbb14b
Shambu1329 <shambukrishna34@gmail.com>
2022-07-02 16:00:28 +0530

    completed

---
+++ food.js
@@ -0,0 +1,27 @@
+foodbtnevent=()=>{
+    Name=(document.getElementById("Name").value)
+    Address=(document.getElementById("Address").value)
+    Pincode=(document.getElementById("Pincode").value)
+    PhoneNo=(document.getElementById("PhoneNo").value)
+    Email=(document.getElementById("Email").value)
+    
+
+    console.log(Name)
+    alert(Name)
+
+
+    console.log(Address)
+    alert(Address)
+
+
+    console.log(Pincode)
+    alert(Pincode)
+
+
+    console.log(PhoneNo)
+    alert(PhoneNo)
+
+
+    console.log(Email)
+    alert(Email)
+}
